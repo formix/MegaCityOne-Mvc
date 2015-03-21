a5e6e656db75347c49b52848ea63cefd888acd4d
formix <jeanphilippe.gravel@gmail.com>
2015-03-21 19:20:55 -0400

    Updated assemblyinfo

--- MegaCityOne.Mvc/Properties/AssemblyInfo.cs
+++ MegaCityOne.Mvc/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("formix.org")]
 [assembly: AssemblyProduct("MegaCityOne.Mvc")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © formix.org 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
